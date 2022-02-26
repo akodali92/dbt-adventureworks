00c8f8f80e1810b728448ceddda831155902eb08
Aneesh Kodali <akodali@analytics8.com>
2022-02-26 19:33:43 +0000

    Add stg_shipmethod.sql

---
+++ models/persistent_staging/purchasing/stg_shipmethod.sql
@@ -0,0 +1,21 @@
+with shipmethod as (
+
+    select * from {{ source ('aw_purchasing', 'shipmethod') }} 
+
+), 
+
+final as (
+
+    select
+        SHIPMETHODID as ship_method_id,
+        NAME,
+        SHIPBASE as ship_base,
+        SHIPRATE as ship_rate,
+        ROWGUID as row_guid,
+        modifieddate as last_update,
+        CURRENT_TIMESTAMP() AS load_date
+    from shipmethod
+
+)
+
+select * from final
