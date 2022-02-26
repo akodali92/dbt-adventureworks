adeea6502bac383b7a9423a66cf0f9dbfcd30171
Aneesh Kodali <akodali@analytics8.com>
2022-02-26 19:55:13 +0000

    Add stg_stateprovince.sql

---
+++ models/persistent_staging/person/stg_stateprovince.sql
@@ -0,0 +1,22 @@
+with stateprovince as (
+
+    select * from {{ source ('aw_person', 'stateprovince') }} 
+
+), 
+
+final as (
+
+    select
+        STATEPROVINCEID as state_province_id,
+        STATEPROVINCECODE as state_province_abbv,
+        COUNTRYREGIONCODE as country_region_abbv,
+        NAME as state_province,
+        TERRITORYID as territory_id,
+        ROWGUID as row_guid,
+        modifieddate as last_update,
+        CURRENT_TIMESTAMP() AS load_date
+    from stateprovince
+
+)
+
+select * from final
