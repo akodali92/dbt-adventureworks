753a3cc84e7da542207723961af2ff9a1d940aaf
Aneesh Kodali <akodali@analytics8.com>
2022-02-26 19:25:32 +0000

    Add stg_employeedepartmenthistory.sql

---
+++ models/persistent_staging/human_resources/stg_employeedepartmenthistory.sql
@@ -0,0 +1,23 @@
+with employeedepartmenthistory as (
+
+    select * from {{ ref ('snapshot_employeedepartmenthistory') }} 
+
+), 
+
+final as (
+
+    select
+        BUSINESSENTITYID || '~' || DEPARTMENTID as employee_department_history_sk,
+        BUSINESSENTITYID as business_entity_id,
+        DEPARTMENTID as department_id,
+        SHIFTID AS shift_id,
+        STARTDATE AS start_date,
+        ENDDATE AS end_date,
+        modifieddate as last_update,
+        dbt_valid_from AS load_date,
+        dbt_valid_to AS load_end_date
+    from employeedepartmenthistory
+
+)
+
+select * from final
