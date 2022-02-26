89904c18601e6a835c3b7c380cfde6f5a5feaa04
Aneesh Kodali <akodali@analytics8.com>
2022-02-26 18:22:52 +0000

    Update snapshot_employeepayhistory unique_key

--- snapshots/snapshot_employeepayhistory.sql
+++ snapshots/snapshot_employeepayhistory.sql
@@ -3,7 +3,7 @@
 {{
     config(
       target_schema='snapshots',
-      unique_key='businessentityid',
+      unique_key='businessentityid||"-"||ratechangedate',
 
       strategy='timestamp',
       updated_at='modifieddate'
